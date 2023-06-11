a9b3309b04142abda11da719b2aa9700b1563591
Matej Tarca <matej.tarca@kiwi.com>
2023-06-11 16:22:03 +0200

    fix(app): fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,37 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import "../global.css";
 import GoogleAnalyticsScript from "../workspaces/web-client/src/scripts/GoogleAnalyticsScript";
 
+import { Jost, Open_Sans, Poppins } from "next/font/google";
+
+const jost = Jost({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+const openSans = Open_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  weight: "500",
+  subsets: ["latin"],
+});
+
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <GoogleAnalyticsScript />
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <style jsx global>{`
+        :root {
+          --font-jost: ${jost.style.fontFamily};
+          --font-open-sans: ${openSans.style.fontFamily};
+          --font-poppins: ${poppins.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </QueryClientProvider>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,10 +20,6 @@ export default function Document() {
         <link href="/assets/img/favicon.png" rel="icon" />
         <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
         <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i&display=optional"
-          rel="stylesheet"
-        />
-        <link
           href="/assets/vendor/bootstrap/css/bootstrap.min.css"
           rel="stylesheet"
         />
